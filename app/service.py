df572282e3d92dd9e501d1af49ead2e441447a5b
julio.santos <julio.nolasco@mail.uft.edu.br>
2024-12-24 09:44:44 -0300

    fix: service do toin + retirada de filtro

--- app/service.py
+++ app/service.py
@@ -1,12 +1,9 @@
 import os
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests
-import django
-import sys
 import environ
-
-from django.utils import timezone
+import json
 from pathlib import Path
 
 env = environ.Env()
@@ -18,7 +15,7 @@ HEADERS = {"Authorization": f"Token {TOKEN}"}
 
 # Pasta de backups
 BASE_DIR = Path(__file__).resolve().parent
-PASTA_BACKUP = BASE_DIR / "backups"
+PASTA_BACKUP = Path("/app/backups")  # Defina a pasta corretamente
 os.makedirs(PASTA_BACKUP, exist_ok=True)
 
 
@@ -51,14 +48,19 @@ def atualizar_data_ultimo_backup():
 def salvar_backup(nome_equipamento, conteudo_backup):
     pasta_equipamento = PASTA_BACKUP / nome_equipamento
     pasta_equipamento.mkdir(parents=True, exist_ok=True)
+    print(f"Salvar aqui: {pasta_equipamento}")
 
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     caminho_arquivo = pasta_equipamento / f"{nome_equipamento}_{timestamp}.txt"
 
-    with open(caminho_arquivo, "w", encoding="utf-8") as f:
-        f.write(conteudo_backup['resultado'])
+    try:
+        # Se o conteúdo for uma string, salva diretamente
+        with open(caminho_arquivo, "w", encoding="utf-8") as f:
+            f.write(conteudo_backup)
+        print(f"Backup salvo em: {caminho_arquivo}")
+    except Exception as e:
+        print(f"Erro ao salvar o backup no caminho {caminho_arquivo}: {e}")
 
-    print(f"Backup salvo em: {caminho_arquivo}")
     return caminho_arquivo
 
 
@@ -90,7 +92,7 @@ def enviar_arquivo_ftp(caminho_arquivo, nome_equipamento):
     """
     servidor_ftp = "177.52.216.4"  # IP do servidor FTP
     usuario_ftp = "backup"
-    senha_ftp = "Anas2108@@1"
+    senha_ftp = "Anas2108@@"
     pasta_destino = f"/{nome_equipamento}/"  # Caminho correto no servidor FTP
     porta_ftp = 21  # Porta do FTP
 
@@ -120,17 +122,17 @@ def enviar_arquivo_ftp(caminho_arquivo, nome_equipamento):
     print(f"Backup {caminho_arquivo} enviado via FTP para {pasta_destino}.")
 
 
-# Atualiza o campo 'ultimo_backup' via API
+# Atualiza o campo 'ultimo_backup' via API no servidor master
 def atualizar_ultimo_backup(equipamento_id):
-    """
-    Atualiza o campo 'ultimo_backup' do equipamento via API.
-    """
-    url = f"/equipments/{equipamento_id}/update_backup/"
-    data_atual = datetime.now().isoformat()  # Formato ISO 8601
+
+    url = f"{API_URL}/equipments/{equipamento_id}/update_backup/"
+    print(f"URL gerada para atualização: {url}")  # Depuração
+    data_atual = datetime.now().isoformat()
     payload = {"ultimo_backup": data_atual}
 
     try:
         response = requests.patch(url, headers=HEADERS, json=payload)
+        print(f"Resposta do servidor: {response.status_code} - {response.text}")
         if response.status_code == 200:
             print(f"Último backup atualizado com sucesso para o equipamento {equipamento_id}. Data enviada: {data_atual}")
         else:
@@ -139,7 +141,6 @@ def atualizar_ultimo_backup(equipamento_id):
         print(f"Erro ao enviar atualização do último backup: {e}")
 
 
-# Função para realizar o backup de um equipamento
 def realizar_backup(equipamento):
     print(f"Iniciando backup para {equipamento['descricao']} ({equipamento['ip']})")
 
@@ -158,34 +159,49 @@ def realizar_backup(equipamento):
             "nome_equipamento": equipamento['descricao'],
             "protocolo": protocolo,
         }
+        # Converte o dicionário em JSON
+        json_data = json.dumps(data)
+        print(json_data)
 
-        resposta = requests.post(f'{API_URL}/service/acessar_equipamento', json=data, headers=HEADERS)
+        # Realiza a chamada da API
+        resposta = requests.post(f'{API_URL}/acessar_equipamento/', json=json_data, headers=HEADERS)
 
-        if resposta:
-            # Salva o backup localmente
-            caminho_arquivo = salvar_backup(equipamento['descricao'], resposta)
-
-            # Envia o backup via FTP
+        # Verifique se a resposta da API foi bem-sucedida
+        if resposta.status_code == 200:
             try:
+                # Converte o texto da resposta para um dicionário
+                conteudo_backup = json.loads(resposta.text)
+                print(f"Conteúdo do backup: {conteudo_backup}")
+
+                # Verifique se o conteúdo da resposta é um dicionário e contém a chave 'resultado'
+                if isinstance(conteudo_backup, dict) and 'resultado' in conteudo_backup:
+                    backup_data = conteudo_backup['resultado']
+                    # Salva o backup localmente
+                    caminho_arquivo = salvar_backup(equipamento['descricao'], backup_data)
+                else:
+                    print("A chave 'resultado' não foi encontrada na resposta ou o conteúdo não é um dicionário.")
+                    # Se não houver a chave 'resultado', salvamos a resposta diretamente
+                    caminho_arquivo = salvar_backup(equipamento['descricao'], conteudo_backup)
+
+                # Envia o backup via FTP
                 enviar_backup(equipamento['id'], caminho_arquivo, equipamento['descricao'])
             except Exception as e:
-                print(f"Erro ao enviar o arquivo para o FTP: {e}")
+                print(f"Erro ao salvar ou enviar o backup: {e}")
 
-            # Atualiza a data do último backup via API
-            try:
-                atualizar_ultimo_backup(equipamento['id'])
-            except Exception as e:
-                print(f"Erro ao atualizar o último backup na API: {e}")
+        # Atualiza a data do último backup via API
+        try:
+            atualizar_ultimo_backup(equipamento['id'])
+        except Exception as e:
+            print(f"Erro ao atualizar o último backup no servidor primário: {e}")
 
-            # Atualiza o controle local de backup
+        # Atualiza o controle local de backup
+        try:
             atualizar_data_ultimo_backup()
-
-        else:
-            print(f"Erro: Resposta vazia para o equipamento {equipamento['descricao']}.")
+        except Exception as e:
+            print(f"Erro ao atualizar o controle local de backup: {e}")
 
     except Exception as e:
         print(f"Erro ao realizar backup: {e}")
-
 
 
 # Função para executar backups de todos os equipamentos
@@ -204,13 +220,14 @@ def executar_backups():
 
         for equipamento in equipamentos:
             realizar_backup(equipamento)
+    else:
+        print(f"Erro ao buscar equipamentos: {response.status_code}")
 
 
 # Obtém o horário agendado via API
 def obter_horario_backup():
     url = f"{API_URL}/enterprises"
     response = requests.get(url, headers=HEADERS)
-    print('teste')
     if response.status_code == 200:
         empresas = response.json()
         if empresas and isinstance(empresas, list):
@@ -226,6 +243,7 @@ def enviar_backup(equipamento_id, caminho_arquivo, nome_equipamento):
     try:
         # Envia via FTP para o diretório correto
         enviar_arquivo_ftp(caminho_arquivo, nome_equipamento)
+
     except Exception as e:
         print(f"Erro no envio via FTP: {e}")
 
@@ -237,13 +255,23 @@ def processar_backups(request):
         print("Não foi possível obter o horário de backup.")
         return
 
-    print(f"Backup agendado para: {horario_agendado}")
     while True:
         # Verifica se o backup já foi realizado hoje
-       # if backup_hoje_realizado():
-       #     print("Backup já realizado hoje. Aguardando amanhã...")
-       #     time.sleep(86400)  # Aguardar 24 horas (86400 segundos) para o próximo dia
-       #     continue  # Reinicia o loop para verificar o horário do novo dia
+        if backup_hoje_realizado():
+            print("Backup já realizado hoje. Aguardando próximo horário...")
+            with open("ultimo_backup.txt", 'r') as f:
+                ultima_execucao = datetime.fromisoformat(f.read().strip())
+
+            agora = datetime.now()
+            proximo_horario = ultima_execucao + timedelta(seconds=86400)  # Próximo horário após 24 horas
+
+            # Calcula o tempo restante até o próximo horário
+            diferenca = (proximo_horario - agora).total_seconds()
+            if diferenca > 0:
+                print(f"Esperando {diferenca // 3600} horas e {diferenca % 3600 // 60} minutos até o próximo backup...")
+                time.sleep(diferenca)
+            continue
+
         agora = datetime.now().strftime("%H:%M:%S")
         print(f"Horário atual: {agora}")
 
